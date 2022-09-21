ef98a78c665f786fa9975e83f1ac882787bf2a6e
Arthur Tavares <arthur001123@gmail.com>
2022-09-21 10:44:10 -0300

    Integrando KY-040

--- src/main.c
+++ src/main.c
@@ -5,11 +5,28 @@
 #include "esp_event.h"
 #include "esp_http_client.h"
 #include "esp_log.h"
+#include "esp_system.h"
 #include "freertos/semphr.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 
 #include "wifi.h"
 #include "http_client.h"
 #include "mqtt.h"
+#include "dht11.h"
+#include "rotary_encoder.h"
+
+#define TAG "MAIN"
+
+#define ROT_ENC_A_GPIO 15
+#define ROT_ENC_B_GPIO 14
+
+rotary_encoder_info_t info = {0};
+
+#define ENABLE_HALF_STEPS false // Set to true to enable tracking of rotary encoder at half step resolution
+#define RESET_AT 0              // Set to a positive non-zero number to reset the position if this value is exceeded
+#define FLIP_DIRECTION false    // Set to true to reverse the clockwise/counterclockwise sense
 
 xSemaphoreHandle conexaoWifiSemaphore;
 xSemaphoreHandle conexaoMQTTSemaphore;
@@ -32,21 +49,84 @@ void conectadoWifi(void *params)
 void trataComunicacaoComServidor(void *params)
 {
     char mensagem[50];
+    struct dht11_reading dht11_info;
     if (xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
     {
         while (true)
         {
-            float temperatura = 20.0 + (float)rand() / (float)(RAND_MAX / 10.0);
-            sprintf(mensagem, "{\"temperatura\": %f}", temperatura);
-            printf("sending %s\n", mensagem);
-            mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
-            vTaskDelay(3000 / portTICK_PERIOD_MS);
+            dht11_info = DHT11_read();
+            printf("Temperature is %d \n", dht11_info.temperature);
+            printf("Humidity is %d\n", dht11_info.humidity);
+            printf("Status code is %d\n", dht11_info.status);
+            if (dht11_info.status == 0)
+            {
+
+                float temperatura = 20.0 + (float)rand() / (float)(RAND_MAX / 10.0);
+                sprintf(mensagem, "{\"temperature\": %d, \"humidity\": %d}", dht11_info.temperature, dht11_info.humidity);
+                printf("sending %s\n", mensagem);
+                mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
+                vTaskDelay(3000 / portTICK_PERIOD_MS);
+            }
+            else
+            {
+                printf("Skipping\n");
+                vTaskDelay(500 / portTICK_PERIOD_MS);
+            }
+        }
+    }
+}
+
+void rotaryReader(void *params)
+{
+    QueueHandle_t event_queue = rotary_encoder_create_queue();
+    ESP_ERROR_CHECK(rotary_encoder_set_queue(&info, event_queue));
+    while (true)
+    {
+        // Wait for incoming events on the event queue.
+        rotary_encoder_event_t event = {0};
+        if (xQueueReceive(event_queue, &event, 1000 / portTICK_PERIOD_MS) == pdTRUE)
+        {
+            ESP_LOGI(TAG, "Event: position %d, direction %s", event.state.position,
+                     event.state.direction ? (event.state.direction == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+        }
+        else
+        {
+            // Poll current position and direction
+            rotary_encoder_state_t state = {0};
+            ESP_ERROR_CHECK(rotary_encoder_get_state(&info, &state));
+            ESP_LOGI(TAG, "Poll: position %d, direction %s", state.position,
+                     state.direction ? (state.direction == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+
+            // Reset the device
+            if (RESET_AT && (state.position >= RESET_AT || state.position <= -RESET_AT))
+            {
+                ESP_LOGI(TAG, "Reset");
+                ESP_ERROR_CHECK(rotary_encoder_reset(&info));
+            }
         }
     }
 }
 
 void app_main(void)
 {
+
+    // esp32-rotary-encoder requires that the GPIO ISR service is installed before calling rotary_encoder_register()
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
+
+    // Initialise the rotary encoder device with the GPIOs for A and B signals
+    ESP_ERROR_CHECK(rotary_encoder_init(&info, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO));
+    ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&info, ENABLE_HALF_STEPS));
+#ifdef FLIP_DIRECTION
+    ESP_ERROR_CHECK(rotary_encoder_flip_direction(&info));
+#endif
+
+    // Create a queue for events from the rotary encoder driver.
+    // Tasks can read from this queue to receive up to date position information.
+    QueueHandle_t event_queue = rotary_encoder_create_queue();
+    ESP_ERROR_CHECK(rotary_encoder_set_queue(&info, event_queue));
+
+    DHT11_init(GPIO_NUM_4);
+
     // Inicializa o NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
@@ -61,6 +141,8 @@ void app_main(void)
     wifi_start();
     printf("wifi_started\n");
 
+    xTaskCreate(&rotaryReader, "Leitura do codificador rotativo", 4096, NULL, 1, NULL);
+    printf("rotaryReader created\n");
     xTaskCreate(&conectadoWifi, "Conexão ao MQTT", 4096, NULL, 1, NULL);
     printf("conectadoWifi created\n");
     xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
